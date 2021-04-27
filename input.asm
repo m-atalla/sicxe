e338a89e3a5a01ebc0e09773c3abed25e4be7699
m-atalla <mo.atalla96@gmail.com>
2021-04-28 00:53:47 +0200

    add test input

---
+++ input.asm
@@ -0,0 +1,9 @@
+        START   0000
+        LDA     FIVE
+        STA     ALPHA
+        LDCH    CHARZ
+        STCH    C1
+ALPHA   RESW    1
+FIVE    WORD    5
+CHARZ   BYTE    C'Z'
+C1      RESB    1       
