dfdae45ab480df36116467b81b8e6c2453fc7195
m-atalla <mo.atalla96@gmail.com>
2021-05-04 17:03:15 +0200

    add pass_one

---
+++ pass_one.py
@@ -0,0 +1,31 @@
+from re import sub
+from line import Line
+
+def locctr_list(asm):
+    """
+    locctr added to each line object
+    """
+    pc = int(asm[0].operand, base=16)
+
+    asm[0].locctr = asm[1].locctr = fhex(pc)
+
+    for line in asm[2:]:
+        if line.op == 'RESB':
+            pc += int(line.operand)
+        elif line.op == 'BYTE':
+            pc += len(line.operand) - 3
+        elif line.op == 'RESW':
+            pc += int(line.operand) * 3
+        elif line.op == 'END':
+            pass
+        else:
+           pc += 3
+           
+        line.locctr = fhex(pc)
+
+
+def create_sym_table(asm):
+    return {x.label:x.locctr for x in asm if x.label}
+        
+def fhex(i):
+    return f'0x{hex(i)[2:].zfill(4)}'
