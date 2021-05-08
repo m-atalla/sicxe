74db4442eba812181c9ada6ae1126e35e8390b8f
m-atalla <mo.atalla96@gmail.com>
2021-05-08 15:06:02 +0200

    add 'END' directive

--- input.asm
+++ input.asm
@@ -1,5 +1,5 @@
         START   0000
-        LDA     FIVE
+FIRST   LDA     FIVE
         STA     ALPHA
         LDCH    CHARZ
         STCH    C1
@@ -7,3 +7,4 @@ ALPHA   RESW    1
 FIVE    WORD    5
 CHARZ   BYTE    C'Z'
 C1      RESB    1       
+        END     FIRST
