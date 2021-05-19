78a38973bb4873d2ff8c635284b617cc04195f27
m-atalla <mo.atalla96@gmail.com>
2021-05-19 22:48:03 +0200

    update `replace_label` to handle index addressing add opcodes reading to dict

--- pass_two.py
+++ pass_two.py
@@ -1,7 +1,21 @@
-def replace_label(asm, sym_table):
+from csv import DictReader
+
+def replace_label(asm, sym_tab):
     for line in asm:
-        if line.operand in sym_table:
-            line.operand = sym_table[line.operand]
+        if line.operand in sym_tab:
+            line.operand = sym_tab[line.operand]
+        elif ',' in line.operand:
+            label, register = line.operand.split(',')
+            label = sym_tab[label]
+            line.operand = f"{label},{register}"
 
-    
 
+def create_objcode(asm):
+    pass
+
+def get_opcodes():
+    opcodes = {}
+    with open('opcodes') as src:
+        reader = DictReader(src)
+        opcodes = {row['mnemonic']:row['opcode'] for row in reader}
+    return opcodes
