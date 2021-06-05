27dcbfa226e9d1039b9d21edd8bd20b6ccf64fca
m-atalla <mo.atalla96@gmail.com>
2021-06-05 02:05:33 +0200

    add 2 parameter args case to Line constructor

--- line.py
+++ line.py
@@ -1,7 +1,12 @@
 class Line:
     def __init__(self, args):
-        self.label, self.mnemonic ,self.operand = args
-        
+
+        if len(args) == 3:
+            self.label, self.mnemonic ,self.operand = args
+        else:
+            self.label, self.mnemonic = args
+            self.operand = None
+            
         self.locctr = None
         self.objcode = None
     
@@ -12,7 +17,7 @@ class Line:
         iasm = fill(self.locctr)
         iasm += fill(self.label)
         iasm += fill(self.mnemonic)
-        iasm += fill(self.operand)
+        iasm += fill(self.operand or '')
         iasm += fill(self.objcode or '') + '\n'
         return iasm
     
@@ -20,4 +25,7 @@ class Line:
         return str(self.__dict__)
 
 def fill(x):
+    """
+    fill empty space padding for each column
+    """
     return x + ' ' * (10 - len(x))
