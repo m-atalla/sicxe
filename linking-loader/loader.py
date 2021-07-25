1d15445392e6d5d17669203d8c69bf35192abd61
m-atalla <mo.atalla96@gmail.com>
2021-07-25 03:46:11 +0200

    modify loader to start at the free address handling modification instructions (WIP)

--- linking-loader/loader.py
+++ linking-loader/loader.py
@@ -1,4 +1,5 @@
 from prog import Prog
+import math
 
 def main():
     # Each row contains 16 addr
@@ -27,9 +28,12 @@ def main():
 
     programs = [prog2, prog3, prog1]
 
-    sym_tab = external_sym_tab(start_addr, programs)
+    ext_sym_tab = external_sym_tab(start_addr, programs)
 
-    #load(memory, programs)
+    load(memory, programs, ext_sym_tab)
+
+    for mem in memory[1026:1040]:
+        print(mem)
 
 def external_sym_tab(hex_start_addr: str, programs: list[Prog]) -> dict[str, str]:
     sym_tab = {}
@@ -39,10 +43,10 @@ def external_sym_tab(hex_start_addr: str, programs: list[Prog]) -> dict[str, str
         
         sym_tab[prog.name] = hex_start_addr
 
-        dec_start_addr = int(hex_start_addr, base=16)
+        dec_start_addr = hex2dec(hex_start_addr)
 
         for symdef in prog.defs:
-            def_addr = dec2hex(int(prog.defs[symdef], base=16) + dec_start_addr)
+            def_addr = dec2hex(hex2dec(prog.defs[symdef]) + dec_start_addr)
             sym_tab[remove_symbol_fill(symdef)] = def_addr
 
         # increment starting addr by prog length 
@@ -54,18 +58,27 @@ def external_sym_tab(hex_start_addr: str, programs: list[Prog]) -> dict[str, str
 def dec2hex(num: int) -> str:
     return hex(num).removeprefix('0x').zfill(6).upper()
 
+def hex2dec(hexa: str) -> int:
+    return int(hexa, base=16)
+
 def remove_symbol_fill(symbol: str) -> str:
     while symbol.startswith('X'):
         symbol = symbol[1:]
     return symbol
 
-def load(memory, progs: list[Prog]) -> None:
+def load(memory, progs: list[Prog], ext_sym_tab) -> None:
     for prog in progs:
+        load_offset = hex2dec(ext_sym_tab[prog.name])
+
+        # Loading obj
         for t_record in prog.text:
             # Converting hex address to decimal
-            row, col = parse_hex_addr(t_record['start'])
+            start_addr = dec2hex(
+                hex2dec(t_record['start']) + load_offset
+            )
 
-            # 2 Hex digits at a time 
+            row, col = parse_hex_addr(start_addr)
+
             while len(t_record['obj']) > 0:
                 # Take first 2 chars
                 memory[row][col] = t_record['obj'][:2]
@@ -73,21 +86,62 @@ def load(memory, progs: list[Prog]) -> None:
                 # remove allocated characters
                 t_record['obj'] = t_record['obj'][2:]
                 
-                col += 1
-                
-                # upon reaching the end of row 
-                # increase row index and reset col
-                if col == 16:
-                    row += 1
-                    col = 0
+                row, col = next_mem_cell(row, col)
 
+        # modifications
+        for mod in prog.mods:
+            mod_offset = hex2dec(mod['start']) 
 
+            mod_addr = dec2hex(mod_offset + load_offset)
 
-def parse_hex_addr(t_record) -> tuple[int, int]:
-    addr = int(t_record['start'], base=16)
+            row, col = parse_hex_addr(mod_addr)
+
+            obj = ''
+
+            while len(obj) < 6:
+                obj = obj + memory[row][col]
+                row, col = next_mem_cell(row, col)
+
+            obj = hex2dec(obj)
+
+            mod_op = mod['change'][2]
+
+            mod_symbol = mod['change'][3:]
+
+            mod_offset = hex2dec(ext_sym_tab[mod_symbol])
+
+            # handle negative mod cases
+            if mod['change'][2] == '-':
+                mod_offset *= -1
+            
+            mod_value = dec2hex(obj + mod_offset)
+
+            print(mod_value)
+            exit()
+
+            # TODO: handle 'indexed' mod that reference index instead of mod
+            # ? should probably handle that in program parsing
+            # ? also I need to handle '05' or '06' modification length
+
+            
+            
+
+def next_mem_cell(row: int, col: int) -> tuple[int, int]:
+    col += 1
+    
+    # upon reaching the end of row 
+    # increase row index and reset col
+    if col == 16:
+        row += 1
+        col = 0
+    
+    return row, col
+
+def parse_hex_addr(hex_addr) -> tuple[int, int]:
+    addr = hex2dec(hex_addr)
     
     # Each row 'memory index' contains 16 address
-    # So in order to find the correct div by 16
+    # So in order to find the correct row div by 16
     row_index = math.floor(addr/16)
 
     # And for finding the correct memory starting col
@@ -97,7 +151,7 @@ def parse_hex_addr(t_record) -> tuple[int, int]:
     hex_out_test = (hex(row_index) + hex(col_index)[2:])[2:].zfill(6).upper()
 
     # ensures that the row and col hex values match the original T record value
-    assert t_record['start'] ==  hex_out_test
+    assert hex_addr == hex_out_test
 
     return row_index, col_index
 
